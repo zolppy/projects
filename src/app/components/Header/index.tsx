a596d338a54e5124f8b4b58edcc9f7d4cc2fc459
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-07-18 17:40:59 -0300

    header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+import { montserrat } from "@/utils/fonts";
+
+export function Header() {
+  return (
+    <header
+      className={`${montserrat.className} bg-gradient-to-r from-[#2563eb] to-[#7c3aed] text-white py-6`}
+    >
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        <div className="mb-6 md:mb-0">
+          <h1 className="text-3xl md:text-4xl font-heading font-bold">
+            Project Showcase
+          </h1>
+          <p className="mt-2 opacity-90">
+            Highlighting my web development journey
+          </p>
+        </div>
+        <div className="flex items-center space-x-4">
+          <a
+            href="https://github.com/zolppy"
+            target="_blank"
+            className="hover:text-blue-200 transition-colors"
+          >
+            <FaGithub className="text-xl" />
+          </a>
+          <a
+            href="https://linkedin.com/in/zolppy"
+            target="_blank"
+            className="hover:text-blue-200 transition-colors"
+          >
+            <FaLinkedin className="text-xl" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+Header.displayName = "Header";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Header } from "@/app/components/Header";
+
 export default function Home() {
-  return <h1 className="text-2xl">Hi</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 Home.displayName = "Home";
