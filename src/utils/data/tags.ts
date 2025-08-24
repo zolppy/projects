f13a34d6f137917fb2f8c45e45008aa02b4c02d2
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-08-24 12:46:04 -0300

    feat: more tags

--- src/utils/data/tags.ts
+++ src/utils/data/tags.ts
@@ -1,125 +1,111 @@
 import { v4 as uuid } from "uuid";
 import { Tag } from "@/utils/types/tag";
 
-const cssTag: Tag = {
-  id: uuid(),
-  title: "CSS3",
-  bgColor: "bg-blue-100",
-  txtColor: "text-blue-800",
-};
-
+/* General */
 const dockerTag: Tag = {
   id: uuid(),
   title: "Docker",
   bgColor: "bg-sky-100",
   txtColor: "text-sky-800",
 };
-
-const framerMotionTag: Tag = {
-  id: uuid(),
-  title: "Framer Motion",
-  bgColor: "bg-purple-100",
-  txtColor: "text-purple-800",
-};
-
-const frontendTag: Tag = {
-  id: uuid(),
-  title: "Front-end",
-  bgColor: "bg-amber-100",
-  txtColor: "text-amber-800",
-};
-
 const gitTag: Tag = {
   id: uuid(),
   title: "Git",
   bgColor: "bg-orange-100",
   txtColor: "text-orange-800",
 };
-
 const githubTag: Tag = {
   id: uuid(),
   title: "Github",
   bgColor: "bg-gray-100",
   txtColor: "text-gray-800",
 };
+const scrumTag: Tag = {
+  id: uuid(),
+  title: "Scrum",
+  bgColor: "bg-emerald-100",
+  txtColor: "text-emerald-800",
+};
 
+/* Front-end */
+const cssTag: Tag = {
+  id: uuid(),
+  title: "CSS3",
+  bgColor: "bg-blue-100",
+  txtColor: "text-blue-800",
+};
+const framerMotionTag: Tag = {
+  id: uuid(),
+  title: "Framer Motion",
+  bgColor: "bg-purple-100",
+  txtColor: "text-purple-800",
+};
+const frontendTag: Tag = {
+  id: uuid(),
+  title: "Front-end",
+  bgColor: "bg-amber-100",
+  txtColor: "text-amber-800",
+};
 const htmlTag: Tag = {
   id: uuid(),
   title: "HTML5",
   bgColor: "bg-red-100",
   txtColor: "text-red-800",
 };
-
-const jsTag: Tag = {
+const javaScriptTag: Tag = {
   id: uuid(),
   title: "JavaScript",
   bgColor: "bg-yellow-100",
   txtColor: "text-yellow-800",
 };
-
 const nextTag: Tag = {
   id: uuid(),
   title: "Next.js",
   bgColor: "bg-slate-100",
   txtColor: "text-slate-800",
 };
-
 const reactHookFormTag: Tag = {
   id: uuid(),
   title: "React Hook Form",
   bgColor: "bg-pink-100",
   txtColor: "text-pink-800",
 };
-
 const reactIconsTag: Tag = {
   id: uuid(),
   title: "React Icons",
   bgColor: "bg-rose-100",
   txtColor: "text-rose-800",
 };
-
 const reactTag: Tag = {
   id: uuid(),
   title: "React.js",
   bgColor: "bg-cyan-100",
   txtColor: "text-cyan-800",
 };
-
 const reactTestingLibraryTag: Tag = {
   id: uuid(),
   title: "React Testing Library",
-  bgColor: "bg-emerald-100",
-  txtColor: "text-emerald-800",
+  bgColor: "bg-lime-100",
+  txtColor: "text-lime-800",
 };
-
-const rrdTag: Tag = {
+const reactRouterDom: Tag = {
   id: uuid(),
   title: "React Router DOM",
   bgColor: "bg-fuchsia-100",
   txtColor: "text-fuchsia-800",
 };
-
-const scrumTag: Tag = {
-  id: uuid(),
-  title: "Scrum",
-  bgColor: "bg-green-100",
-  txtColor: "text-green-800",
-};
-
 const tailwindTag: Tag = {
   id: uuid(),
   title: "Tailwind CSS",
   bgColor: "bg-teal-100",
   txtColor: "text-teal-800",
 };
-
-const typescriptTag: Tag = {
+const typeScriptTag: Tag = {
   id: uuid(),
   title: "TypeScript",
   bgColor: "bg-indigo-100",
   txtColor: "text-indigo-800",
 };
-
 const useFormTag: Tag = {
   id: uuid(),
   title: "useForm",
@@ -127,25 +113,186 @@ const useFormTag: Tag = {
   txtColor: "text-violet-800",
 };
 
+/* Machine Learning */
+const artificialIntelligenceTag: Tag = {
+  id: uuid(),
+  title: "Artificial Inteligence",
+  bgColor: "bg-green-100",
+  txtColor: "text-green-800",
+};
+const computerVisionTag: Tag = {
+  id: uuid(),
+  title: "Computer Vision",
+  bgColor: "bg-blue-100",
+  txtColor: "text-blue-800",
+};
+const convolutionalNetworksTag: Tag = {
+  id: uuid(),
+  title: "Convolutional Networks",
+  bgColor: "bg-purple-100",
+  txtColor: "text-purple-800",
+};
+const deepLearningTag: Tag = {
+  id: uuid(),
+  title: "Deep Learning",
+  bgColor: "bg-red-100",
+  txtColor: "text-red-800",
+};
+const hyperparameterTunningTag: Tag = {
+  id: uuid(),
+  title: "Hyperparameter Tuning",
+  bgColor: "bg-yellow-100",
+  txtColor: "text-yellow-800",
+};
+const imageProcessingTag: Tag = {
+  id: uuid(),
+  title: "Image Processing",
+  bgColor: "bg-indigo-100",
+  txtColor: "text-indigo-800",
+};
+const kaggleTag: Tag = {
+  id: uuid(),
+  title: "Kaggle",
+  bgColor: "bg-pink-100",
+  txtColor: "text-pink-800",
+};
+const kerasTag: Tag = {
+  id: uuid(),
+  title: "Keras",
+  bgColor: "bg-teal-100",
+  txtColor: "text-teal-800",
+};
+const machineLearningTag: Tag = {
+  id: uuid(),
+  title: "Machine Learning",
+  bgColor: "bg-orange-100",
+  txtColor: "text-orange-800",
+};
+const modelEvaluationTag: Tag = {
+  id: uuid(),
+  title: "Model Evaluation",
+  bgColor: "bg-cyan-100",
+  txtColor: "text-cyan-800",
+};
+const naturalLanguageProcessingTag: Tag = {
+  id: uuid(),
+  title: "Natural Language Processing",
+  bgColor: "bg-lime-100",
+  txtColor: "text-lime-800",
+};
+const neuralNetworksTag: Tag = {
+  id: uuid(),
+  title: "Neural Networks",
+  bgColor: "bg-amber-100",
+  txtColor: "text-amber-800",
+};
+const objectClassificationTag: Tag = {
+  id: uuid(),
+  title: "Object Classification",
+  bgColor: "bg-emerald-100",
+  txtColor: "text-emerald-800",
+};
+const objectIdentificationTag: Tag = {
+  id: uuid(),
+  title: "Object Identification",
+  bgColor: "bg-violet-100",
+  txtColor: "text-violet-800",
+};
+const objectSegmentationTag: Tag = {
+  id: uuid(),
+  title: "Object Segmentation",
+  bgColor: "bg-fuchsia-100",
+  txtColor: "text-fuchsia-800",
+};
+const openCvTag: Tag = {
+  id: uuid(),
+  title: "OpenCV",
+  bgColor: "bg-rose-100",
+  txtColor: "text-rose-800",
+};
+const promptEngineeringTag: Tag = {
+  id: uuid(),
+  title: "Prompt Engineering",
+  bgColor: "bg-sky-100",
+  txtColor: "text-sky-800",
+};
+const pythonTag: Tag = {
+  id: uuid(),
+  title: "Python",
+  bgColor: "bg-amber-100",
+  txtColor: "text-amber-800",
+};
+const retrievalAugmentedGenerationTag: Tag = {
+  id: uuid(),
+  title: "Retrieval Augmented Generation",
+  bgColor: "bg-indigo-100",
+  txtColor: "text-indigo-800",
+};
+const scikitLearnTag: Tag = {
+  id: uuid(),
+  title: "Scikit-Learn",
+  bgColor: "bg-orange-100",
+  txtColor: "text-orange-800",
+};
+const tensorFlowTag: Tag = {
+  id: uuid(),
+  title: "TensorFlow",
+  bgColor: "bg-red-100",
+  txtColor: "text-red-800",
+};
+const transferLearningTag: Tag = {
+  id: uuid(),
+  title: "Transfer Learning",
+  bgColor: "bg-green-100",
+  txtColor: "text-green-800",
+};
+
 const tags = {
-  cssTag: cssTag,
-  dockerTag: dockerTag,
-  framerMotionTag: framerMotionTag,
-  frontendTag: frontendTag,
-  gitTag: gitTag,
-  githubTag: githubTag,
-  htmlTag: htmlTag,
-  jsTag: jsTag,
-  nextTag: nextTag,
-  reactHookFormTag: reactHookFormTag,
-  reactIconsTag: reactIconsTag,
-  reactTag: reactTag,
-  reactTestingLibraryTag: reactTestingLibraryTag,
-  rrdTag: rrdTag,
-  scrumTag: scrumTag,
-  tailwindTag: tailwindTag,
-  typescriptTag: typescriptTag,
-  useFormTag: useFormTag,
+  // General
+  dockerTag,
+  gitTag,
+  githubTag,
+  scrumTag,
+
+  // Front-end
+  cssTag,
+  framerMotionTag,
+  frontendTag,
+  htmlTag,
+  javaScriptTag,
+  nextTag,
+  reactHookFormTag,
+  reactIconsTag,
+  reactTag,
+  reactTestingLibraryTag,
+  reactRouterDom,
+  tailwindTag,
+  typeScriptTag,
+  useFormTag,
+
+  // Machine Learning
+  artificialIntelligenceTag,
+  computerVisionTag,
+  convolutionalNetworksTag,
+  deepLearningTag,
+  hyperparameterTunningTag,
+  imageProcessingTag,
+  kaggleTag,
+  kerasTag,
+  machineLearningTag,
+  modelEvaluationTag,
+  naturalLanguageProcessingTag,
+  neuralNetworksTag,
+  objectClassificationTag,
+  objectIdentificationTag,
+  objectSegmentationTag,
+  openCvTag,
+  promptEngineeringTag,
+  pythonTag,
+  retrievalAugmentedGenerationTag,
+  scikitLearnTag,
+  tensorFlowTag,
+  transferLearningTag,
 };
 
 export { tags };
